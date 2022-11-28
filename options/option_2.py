ee4fb6f36ff907223dee8ad4b0d114d21903db02
FatGuyy <omkarjadhav2103@gmail.com>
2022-11-28 17:11:18 +0530

    added the 2nd csv for 2nd csv.

--- options/option_2.py
+++ options/option_2.py
@@ -28,7 +28,7 @@ def option_2(FILE_NAMES):
     for i in FILE_NAMES:
         file_ele = i.split('_')
         First_name = file_ele[0].title()    # This extracts the first element
-        Last_name = file_ele[1].title()     # This extracts the last element
+        Last_name = file_ele[1].title()     # This extracts the second element
         Last_4 = file_ele[-1]
 
         # product list
@@ -57,3 +57,51 @@ def option_2(FILE_NAMES):
         name_link.append((First_name+"-"+Last_name))
 
     return [product_list, sku_list, price, Sale_price, photo_list, video_list, name_list, name_link]
+
+
+def option_2_2nd_csv(FILE_NAMES):
+    product_list = []
+    sku_list = []
+    price =[]
+    column_d = []
+    column_e = []
+    column_f = []
+    column_g = []
+    last_sku =[]
+
+        
+    product_name = "{} {} signed model B 8×10 Photo -PROOF- -CERTIFICATE-  (A{})"
+    date1 = _date()
+    
+    for i in FILE_NAMES:
+        file_ele = i.split('_')
+        First_name = file_ele[0].title()    # This extracts the first element
+        Last_name = file_ele[1].title()     # This extracts the second element
+        Last_4 = file_ele[-1]
+
+        # name
+        product_list.append(product_name.format(First_name,Last_name, Last_4))
+
+        # sku list
+        sku = First_name + Last_name[0] + f"{Last_4}" + date1 + "rb"
+        sku_list.append(sku)
+         
+        # price
+        price.append(29.95)
+
+        # column d
+        column_d.append(1)
+
+        # column e
+        column_e.append("")
+
+        # column f
+        column_f.append(39)
+
+        # column g
+        column_g.append('')
+
+        # sku last part
+        last_sku.append("rb")
+
+    return [product_list, sku_list, price, column_d, column_e, column_g, last_sku]
